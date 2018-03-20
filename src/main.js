69c9a382352dce9ca70c36592dbf77a4ae93e91d
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2018-03-20 18:01:36 -0400

    Modulo de Experiencia Laboral

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/vendor/devicons/css/devicons.min.css'
 import './assets/vendor/simple-line-icons/css/simple-line-icons.css'
 import './assets/css/resume.min.css'
 
+//import './assets/vendor/jquery/jquery.min'
+//import './assets/vendor/popper/popper'
+//import './assets/vendor/bootstrap/js/bootstrap.min'
+//import './assets/vendor/jquery-easing/jquery.easing.min'
+//import './assets/js/resume.min'
+
 Vue.config.productionTip = false
 
 new Vue({
